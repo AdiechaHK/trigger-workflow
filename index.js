6b28e265623ae30d4db8dd4629fae64913b4e711
Harikrushna <adiechahari@gmail.com>
2023-02-02 11:29:24 +0530

    checking

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 (
     async () => {
         try {
+            console.log(process.env);
             core.info('Yes here in my action [trigger workflow]')
         } catch (error) {
             core.setFailed(error.message);
