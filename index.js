b77262dcda8960d2bda2ae06844fcc6e494b0faa
Harikrushna <adiechahari@gmail.com>
2023-02-02 13:45:43 +0530

    added fetch

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 (
     async () => {
         try {
-            const url = "https://api.github.com/repos/AdiechaHK/test-folder-sync-2/dispatches"
+            const path = process.env.INPUT_REPO_PATH;
+            const token = process.env.INPUT_REPO_TOKEN;
+            const url = `https://api.github.com/repos/${path}/dispatches`;
             await fetch(url, {
                 method: 'POST',
                 headers: {
                     "Accept": "application/vnd.github+json",
                     "X-GitHub-Api-Version": "2022-11-28",
-                    "Authorization": "Bearer " + process.env.INPUT_REPO_TOKEN
+                    "Authorization": `Bearer ${token}`
                 },
                 body: JSON.stringify({"event_type":"sync-folder"})
             });
